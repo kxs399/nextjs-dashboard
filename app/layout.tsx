b3dfeff761f97efb75ed107145de1937449edaa0
Kiran Sripada <>
2024-07-16 19:01:18 -0400

    initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,12 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" className={inter.className}>
+      <body>
+        <div>{children}</div>
+      </body>
     </html>
-  );
+  )
 }
